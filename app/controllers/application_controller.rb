1a984a01e1f82e7c710af3e30ceace8e20316804
nnickd <nicolas.dellitalia@gmail.com>
2016-12-03 04:35:37 -0500

    updated application controller and routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user, :create_user
+  layout :decide_layout
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def create_user(params)
+    @user = User.new(params)
+  end
+
+  def decide_layout
+    current_user ? "logged_in" : "application"
+  end
+
+  def require_logged_in
+    return true if current_user
+    return redirect_to root_path
+  end
 end
